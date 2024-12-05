d457899f3cb131331bb96f171ff7d47a0f914692
Victoria <tovictoriadao@gmail.com>
2024-12-05 14:30:40 -0500

    Added a counter variable that allows the text to change depending on the buttons clicked

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.collection.mutableIntObjectMapOf
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -42,9 +44,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Counter() {
-    Column {
-        Button (onClick = {}) {}
+    var counter by rememberSaveable { mutableIntStateOf(0) }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {counter++}
+        ) {
+            Text("Increase")
+        }
+
+        Text(
+            counter.toString(),
+            fontSize = 50.sp
+        )
+
+        Button(onClick = {counter--}) { Text("Decrease") }
     }
+
+
 }
 
 @Preview(showBackground = true)
